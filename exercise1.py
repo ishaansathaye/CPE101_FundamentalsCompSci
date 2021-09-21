1ceb23bd73db4192f419af28cb9ff4ee8f5438ba
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-09-21 13:44:55 -0700

    day 1 excercises

---
+++ exercise1.py
@@ -0,0 +1,22 @@
+#a
+print()
+str1 = "Hello"
+print("str1: " + str1)
+str2 = str1
+
+str1 = input("Enter a new string: ")
+print("str2: " + str2)
+print("str1: " + str1)
+
+#b
+print()
+x = 10
+print("X: " + str(x))
+
+y = x
+x = 1000
+
+print("X: " + str(x))
+print("Y: " + str(y))
+
+
