04eaf7ebcce8c88a06f2f7744c49d9394da966ea
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-09-23 13:59:09 -0700

    changes

--- Lab1-Learning Fundamentals/task2.py
+++ Lab1-Learning Fundamentals/task2.py
@@ -1,8 +1,8 @@
 print()
-x1 = int(input("Enter x1 coordinate: "))
-y1 = int(input("Enter y1 coordinate: "))
-x2 = int(input("Enter x2 coordinate: "))
-y2 = int(input("Enter y2 coordinate: "))
+x1 = float(input("Enter x1 coordinate: "))
+y1 = float(input("Enter y1 coordinate: "))
+x2 = float(input("Enter x2 coordinate: "))
+y2 = float(input("Enter y2 coordinate: "))
 
 distance = (pow((x2-x1), 2)+pow((y2-y1), 2)) ** (1/2)
 print()
