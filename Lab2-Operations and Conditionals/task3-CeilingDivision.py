57fc6eb848307d4d793a1b74f9d5a5ad0b4f3883
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-09-28 13:54:43 -0700

    changes

--- Lab2-Operations and Conditionals/task3-CeilingDivision.py
+++ Lab2-Operations and Conditionals/task3-CeilingDivision.py
@@ -1,13 +1,16 @@
 def ceiling(x, y):
-    quotient = int(x) // int(y)
-    quotient = quotient + 1
+    if ((x % y) == 0):
+        quotient = x / y
+    else:
+        quotient = int(x) // int(y)
+        quotient = quotient + 1
     return quotient
 
 print()
 number1 = input("Enter the numerator: ")
 number2 = input("Enter the denominator: ")
 print()
-print("Ceiling:", ceiling(number1, number2))
+print("Ceiling:", ceiling(int(number1), int(number2)))
 print("Division:", (int(number1)/int(number2)))
 print("Floor:", (int(number1) // int(number2)))
 print()
