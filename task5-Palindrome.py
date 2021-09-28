b4cd1e987cc39ce28b7283fa730a5f5a29860f64
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-09-28 12:57:49 -0700

    Finished Lab 2

---
+++ task5-Palindrome.py
@@ -0,0 +1,19 @@
+print()
+user_string = input("Enter a 5-letter word: ")
+print()
+
+listString = list(user_string)
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+stringCheck = any(i in listString for i in numbers)
+
+if (len(user_string) != 5) or stringCheck == True:
+  print('Error')
+  print()
+else:
+  reverseString = user_string[::-1]
+  print("Reverse of String:", reverseString)
+  print()
+  if user_string == reverseString:
+    print(user_string, "is a palindrome!")
+    print()
