3083e34578a494f781979e2914b778825a8d245f
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-07 23:38:35 -0700

    cleaned up code

--- Lab3-Conditionals and Functions/task3-LowerEmailGenerator.py
+++ Lab3-Conditionals and Functions/task3-LowerEmailGenerator.py
@@ -10,7 +10,6 @@ for i in range(0,3):
       position = "second"
     else:
       position = "third"
-    # prompt = "Enter the", position, "name (first, last): "
     nameInput = input("Enter the " + position + " name (first, last): ")
     names.append(nameInput)
  
