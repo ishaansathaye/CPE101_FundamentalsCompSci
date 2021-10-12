1f892f9da8b1d1e00526895854919f465a712ec3
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-12 14:49:05 -0700

    fib

--- Lab4-Functions and Classes/task2-FactorialFibonacci.py
+++ Lab4-Functions and Classes/task2-FactorialFibonacci.py
@@ -1,16 +1,25 @@
-#Factorial Recursion
-def factorial(x):
-    if x == 1:
-        return x
-    return x*factorial(x-1)
+# #Factorial Recursion
+# def factorial(x):
+#     if x == 1:
+#         return x
+#     return x*factorial(x-1)
 
-print()
-number = int(input("Enter a number: "))
-if number < 0:
-    print("ERROR: Negative number!")
-elif number == 0:
-    print("1")
-else:
-    print(str(number) + "! is", factorial(number))
+# print()
+# number = int(input("Enter a number: "))
+# if number < 0:
+#     print("ERROR: Negative number!")
+# elif number == 0:
+#     print("1")
+# else:
+#     print(str(number) + "! is", factorial(number))
 
 #Fibonacci Sequence Recursion
+def fibonacci(N):
+    if N <= 1:
+        return N
+    return fibonacci(N-1)+fibonacci(N-2)
+
+for i in range(0,10):
+    print(fibonacci(i))
+
+print(fibonacci(3))
