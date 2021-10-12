747afa31d6de8899123beea6bd8d67414d25ab9c
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-12 16:08:56 -0700

    add collision check

--- Lab4-Functions and Classes/task3-3DCollision.py
+++ Lab4-Functions and Classes/task3-3DCollision.py
@@ -1,27 +1,38 @@
+import math
+
 class Sphere:
-    def __init__(self, x, y, z, radius) -> None:
-        self.x = x
-        self.y = y
-        self.z = z
-        self.rad = radius
-    def volumeCalc():
-        pass
+    def __init__(self, position):
+        self.x = float(input("X-coord of the " + position + " sphere: "))
+        self.y = float(input("Y-coord of the " + position + " sphere: "))
+        self.z = float(input("Z-coord of the " + position + " sphere: "))
+        self.rad = float(input("Radius of the " + position + " sphere: "))
+    def volumeCalc(self):
+        return round((4/3)*(math.pi)*(pow(self.rad, 3)), 2)
+    def collision(self, s2x, s2y, s2z, s2rad):
+        distance = round(math.sqrt(((self.x-s2x)**2)+((self.y-s2y)**2)+((self.z-s2z)**2)), 2)
+        if distance <= (self.rad + s2rad):
+            return True
+        else:
+            return False
 
 print()
 print("This program will test if two objects are colliding.")
 print()
 
-print("Enter the details for the first sphere:")
-sphere1 = Sphere()
-sphere1.x = float(input("X-coord of the 1st sphere: "))
-sphere1.y = float(input("Y-coord of the 1st sphere: "))
-sphere1.z = float(input("Z-coord of the 1st sphere: "))
-sphere1.rad = float(input("Radius of the 1st sphere: "))
+print("Enter the details for the 1st sphere:")
+sphere1 = Sphere("1st")
 
-print("Enter the details for the second sphere:")
-sphere2 = Sphere()
-sphere2.x = float(input("X-coord of the 2nd sphere: "))
-sphere2.y = float(input("Y-coord of the 2nd sphere: "))
-sphere2.z = float(input("Z-coord of the 2nd sphere: "))
-sphere2.rad = float(input("Radius of the 2nd sphere: "))
+print()
+print("Enter the details for the 2nd sphere:")
+sphere2 = Sphere("2nd")
 
+print()
+print()
+if sphere1.volumeCalc() == sphere2.volumeCalc():
+    print("The 2 Spheres are IDENTICAL with a Volume of", sphere2.volumeCalc())
+elif sphere1.collision(sphere2.x, sphere2.y, sphere2.z, sphere2.rad) == True:
+    print("The Spheres COLLIDE!")
+else:
+    print("The Spheres do not collide!")
+
+print()
