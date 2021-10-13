8cd82b73fac96e9a0397b99d4bf05e7c6801b467
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-12 21:50:44 -0700

    renamed files

---
+++ Lab4_Functions and Classes/task2_EvenModuleFolder/task2_EvenModule.py
@@ -0,0 +1,17 @@
+def is_even(num):
+    while True:
+        try:
+            if num == float(num):
+                break
+        except ValueError:
+            print()
+            print("Please enter a number only!")
+            num = int(input("Enter a number: "))
+            print()
+            continue
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
---
+++ Lab4_Functions and Classes/task2_EvenModuleFolder/task2_ImportEvenModule.py
@@ -0,0 +1,7 @@
+from task2_EvenModule import is_even
+
+print()
+inputNumber = input("Enter a number: ")
+evenOrOdd = is_even(inputNumber)
+
+print(evenOrOdd)
