4125bc49efa7f594f0c6b96ced4641ebffc0395f
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-19 19:13:21 -0700

    changes

--- Lab5_ClassesandUnitTesting/task2_CalcUnitTests.py
+++ Lab5_ClassesandUnitTesting/task2_CalcUnitTests.py
@@ -1,12 +1,12 @@
-import CPE-101-FundamentalsCompSci.Lab4_FunctionsandClasses.task1_FunctionCalc as FunctionCalc
+from CPE101_FundamentalsCompSci.Lab4_FunctionsandClasses.task1_FunctionCalc import add, subtract, mult, div
 
 import unittest
 
 class TestFunctionCalc(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(FunctionCalc.add(2,10), 12, "Should be 12")
-        self.assertEqual(FunctionCalc.add(2,-10), -8, "Should be 12")
+        self.assertEqual(add(2,10), 12, "Should be 12")
+        self.assertEqual(add(2,-10), -8, "Should be 12")
 
     def test_subtract(self):
         pass
