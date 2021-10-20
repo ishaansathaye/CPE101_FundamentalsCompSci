ac95c2cea17c37cb3c89890025c0ac216df3e1b5
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-19 18:54:32 -0700

    changes

--- Lab5_ClassesandUnitTesting/task2_CalcUnitTests.py
+++ Lab5_ClassesandUnitTesting/task2_CalcUnitTests.py
@@ -1,1 +1,21 @@
-from Lab4_FunctionsandClasses import task2_CalcUnitTests
+import Lab4_FunctionsandClasses.task1_FunctionCalc as FunctionCalc
+
+import unittest
+
+class TestFunctionCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(FunctionCalc.add(2,10), 12, "Should be 12")
+        self.assertEqual(FunctionCalc.add(2,-10), -8, "Should be 12")
+
+    def test_subtract(self):
+        pass
+    
+    def test_mult(self):
+        pass
+
+    def test_div(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
