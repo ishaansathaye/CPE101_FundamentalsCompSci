4709bde9eee5f206c92cfa5a5955209befad7196
ishaansathaye <ishaan.sathaye@gmail.com>
2021-11-16 08:58:06 -0800

    finish task1

--- Lab9_LoopsNestedLoops/task1_HeartRateFolder/task1_HeartRate.py
+++ Lab9_LoopsNestedLoops/task1_HeartRateFolder/task1_HeartRate.py
@@ -10,18 +10,19 @@ def calculate_average_heart_rates(heartRates):
         avgHeartRate.append(average)
     return avgHeartRate
 
+print()
 print(calculate_average_heart_rates(heart_rate))
 print()
 
 def test_calcAverageHeartRate():
     heart_rate = [[ 72, 75, 71, 73]]
-    assert calculate_average_heart_rates(heart_rate) == 72.75, "Should be 72.75"
+    assert calculate_average_heart_rates(heart_rate) == [72.75], "Should be 72.75"
     heart_rate = [[ 91, 90, 94, 93]]
-    assert calculate_average_heart_rates(heart_rate) == 92.0, "Should be 92"
+    assert calculate_average_heart_rates(heart_rate) == [92.0], "Should be 92"
     heart_rate = [[ 130, 135, 139, 142]]
-    assert calculate_average_heart_rates(heart_rate) == 136.5, "Should be 136.5"
+    assert calculate_average_heart_rates(heart_rate) == [136.5], "Should be 136.5"
     heart_rate = [[ 120, 118, 110, 105, 100, 98]]
-    assert calculate_average_heart_rates(heart_rate) == 108.5, "Should be 108.5"
+    assert calculate_average_heart_rates(heart_rate) == [108.5], "Should be 108.5"
 
 test_calcAverageHeartRate()
 print("Everything Passed.")
