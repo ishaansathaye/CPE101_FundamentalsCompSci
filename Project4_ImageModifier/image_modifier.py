3c4f02b3913f9f6c00efc009cb73b51a8729ce94
ishaansathaye <ishaan.sathaye@gmail.com>
2021-11-18 09:09:43 -0800

    update

--- Project4_ImageModifier/image_modifier.py
+++ Project4_ImageModifier/image_modifier.py
@@ -1,41 +1,50 @@
-def negate_image():
+def negate_image(input, output):
     pass
 
-def high_contrast_image():
+def high_contrast_image(input, output):
     pass
 
-def gray_scale_image():
+def gray_scale_image(input, output):
     pass
 
-def remove_red_image():
+def remove_red_image(input, output):
     pass
 
-def remove_green_image():
+def remove_green_image(input, output):
     pass
 
-def remove_blue_image():
+def remove_blue_image(input, output):
     pass
 
+print()
 inputFile = input("Enter name of input PPM file (test.txt): ")
 outputFile = input("Enter name of output file (output.txt): ")
-chosenFunc = input("Enter letter of following commands:\n A. negate\n B. high contrast\n C. gray scale\n D. remove <color> where <color> is red, green, or blue\n Enter here: ")
+chosenFunc = input("Enter letter of following commands:\n A. negate\n B. high contrast\n C. gray scale\n D. remove <color> where <color> is red, green, or blue\nEnter here: ")
 try:
     if chosenFunc.lower() == 'a':
-        negate_image()
+        negate_image(inputFile, outputFile)
     elif chosenFunc.lower() == 'b':
-        high_contrast_image()
+        high_contrast_image(inputFile, outputFile)
     elif chosenFunc.lower() == 'c':
-        gray_scale_image()
+        gray_scale_image(inputFile, outputFile)
     elif chosenFunc.lower() == 'd':
         colorToRemove = input("Which color would you like to remove? (R, G, B")
         try:
             if colorToRemove.lower() == 'r':
-                remove_red_image()
+                remove_red_image(inputFile, outputFile)
             elif colorToRemove.lower() == 'g':
-                remove_green_image()
+                remove_green_image(inputFile, outputFile)
             elif colorToRemove.lower() == 'b':
-                remove_blue_image()
+                remove_blue_image(inputFile, outputFile)
+            else:
+                print()
+                print("Not a valid color option!")
         except:
+            print()
             print("Not a valid color option!")
+    else:
+        print()
+        print("Not a valid option!")
 except:
+    print()
     print("Not a valid command option!")
