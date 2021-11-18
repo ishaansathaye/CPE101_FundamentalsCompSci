a963ae5df511a4478962852935eba962cfed0409
ishaansathaye <ishaan.sathaye@gmail.com>
2021-11-18 08:43:13 -0800

    project 4

---
+++ Project4_ImageModifier/image_modifier.py
@@ -0,0 +1,3 @@
+inputFile = input("Enter name of input PPM file (test.txt): ")
+outputFile = input("Enter name of output file (output.txt): ")
+chosenFunc = input("Enter letter of following commands:\nA.")
